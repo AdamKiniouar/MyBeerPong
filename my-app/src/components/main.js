2d434fa895ae9e6d9f2e7e2c8d891cbecdbf87e5
Adam <Adam.kiniouar@hyhs.se>
2020-01-22 14:57:10 +0100

    Lagt till paths + bild till omoss

--- my-app/src/components/main.js
+++ my-app/src/components/main.js
@@ -1,3 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import HomePage from './homepage';
+import OmOss from './omoss';
+
+
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/omoss" component={OmOss} />
+    </Switch>
+)
+
+export default Main;
