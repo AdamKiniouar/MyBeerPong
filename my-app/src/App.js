eefdda1f23a6af78beb260ab3b7f61bc0bb2bf44
Adam <Adam.kiniouar@hyhs.se>
2020-01-27 13:41:08 +0100

    Lagt till sidor och switchen mellan sidor samt test bilder till om oss sidan.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,4 +2,47 @@ import React, { Component} from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer,Footer,FooterSection,FooterLinkList, Content } from 'react-mdl';
 import Main from './components/main';
+import { Link } from 'react-router-dom';
 
+//https:api.themoviedb.org/3/movie/76341?api_key=334afa804965b7ed56ac1af164c1f215
+
+class App extends Component {
+  render(){
+    return (
+<div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyBeerPong</Link>} scroll>   
+            <Navigation>
+                <Link to="/boka">Boka</Link>
+                <Link to="/regler">Regler</Link>
+                <Link to="/omoss">Om Oss</Link>
+            </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}}  to="/">BilloGrafen</Link>}>
+            <Navigation>
+                <Link to="/boka">Boka</Link>        
+                <Link to="/regler">Regler</Link>
+                <Link to="/omoss">Om Oss</Link>
+            </Navigation>
+        </Drawer>
+
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+
+        <Footer className="footer" size="mini">
+            <FooterSection>
+                <FooterLinkList>
+                     <a href="https://elev.skolinspektionen.se/globalassets/bilder/full-bredd/elevwebben/hjalp.png">Hjälp</a>
+                     <a href="http://powertrainnorden.se/wp-content/uploads/2018/04/Powertrain-villkor.jpg">Intergritet & Villkor</a>
+                </FooterLinkList>
+            </FooterSection>
+        </Footer>
+    </Layout>
+</div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/omoss.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './omoss.css';
+
+
+class OmOss extends Component {
+    render() {
+        return (
+        <div className="home-grid">
+            <img src="owl.jpg" width="100%" height="40%"/>
+            <h3>Här är vi!</h3>
+            <a style={{marginRight: 500}}><img src="Adam cropped.jpg"/></a>
+            <a><img src="Adde cropped.jpg"/></a>
+            
+        
+            </div>
+        )
+    }
+}
+
+export default OmOss;
